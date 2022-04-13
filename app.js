fdb9767f4ca85f40b36e42da76a833121a5968fe
Guan <kuanui21@gmail.com>
2022-04-13 17:55:56 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') // 載入 mongoose
 
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('This is restaurant list page.')
